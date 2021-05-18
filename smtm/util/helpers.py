64f2c54e0fab6b7e81a84083002c3cb524994374
Keith Harrigian <keithharrigian@gmail.com>
2021-05-18 10:32:34 -0400

    Add directory creation utility

--- smtm/util/helpers.py
+++ smtm/util/helpers.py
@@ -3,9 +3,24 @@
 Common helper functions
 """
 
+## Imports
+import os
+
 def flatten(items):
     """
 
     """
     items_flat = [j for i in items for j in i]
     return items_flat
+
+def make_directory(directory,
+                   remove_existing=False):
+    """
+
+    """
+    ## Remove Existing if Desired
+    if os.path.exists(directory) and remove_existing:
+        _ = os.system("rm -rf {}".format(directory))
+    ## Create
+    if not os.path.exists(directory):
+        _ = os.makedirs(directory)
